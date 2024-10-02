893b27a5e0df942610ca7d9be92992e8997f756b
Zohidjon <zohidjonmarufov2008@gmail.com>
2024-10-02 18:41:07 +0500

    Added new project | 02-Js library.

---
+++ 02-js-library/JS/Book.js
@@ -0,0 +1,7 @@
+function Book(title, author, pages, desc, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.desc = desc;
+  this.isRead = isRead;
+}
--- 02-js-library/script.js
+++ 02-js-library/script.js
@@ -1,9 +1,47 @@
+// Input
+const inputTitle = document.querySelector(".inputTitle");
+const inputAuthor = document.querySelector(".inputAuthor");
+const inputPages = document.querySelector(".inputPages");
+const textareaDesc = document.querySelector(".textareaDesc");
+const inputRadioButtons = document.querySelectorAll("input[name='have-read']");
+const inputAddCard = document.querySelector("button.input-add-card");
+
+// Card
+const cardTitle = document.querySelector(".card-title h1");
+const cardDesc = document.querySelector(".card-short-desc p");
+const cardPages = document.querySelector(".card-pages h4");
+const cardIsRead = document.querySelector(".card-have-read h4");
+
+// Main Array
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
+// Events
+inputAddCard.addEventListener("click", () => {
+  let selectedStatus;
+  for (const radioButton of inputRadioButtons) {
+    if (radioButton.checked) {
+      selectedStatus = radioButton.value;
+      break;
+    }
+  }
+  const newBook = new Book(
+    inputTitle.value,
+    inputAuthor.value,
+    inputPages.value,
+    textareaDesc.value,
+    selectedStatus
+  );
+  addBookToLibrary(
+    newBook.title,
+    newBook.author,
+    newBook.pages,
+    newBook.desc,
+    newBook.isRead
+  );
+  myLibrary.push(newBook);
 
-function addBookToLibrary() {
-  // do stuff here
-}
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputPages.value = "";
+  textareaDesc.value = "";
+});
